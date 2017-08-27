fd79bbf921ab01cf9a8bab753bda8fe1e090488e
vlaurenzano <vincent.laurenzano@gmail.com>
2017-08-27 13:09:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = "f5",
     version = "0.0.1",
     packages = find_packages(),
-
+    download_url='https://github.com/vlaurenzano/f5/archive/v0.0.1.tar.gz',
     # metadata for upload to PyPI
     author = "Brendan Berg",
     author_email = "info@plusminusfive.com",
